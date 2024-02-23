18eac36c85de9a2f8505f05941c99722a58ae42e
hllshiro <lihuale@senjone.com>
2024-02-23 08:48:34 +0800

    通过命令行参数传递包名

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
  * 请确保路径 ./download 和 ./download/downloaded 存在
  * 若package.json中有冲突，请将force设置为true，追加'--legacy-peer-deps'参数
  */
-const target = '';
+const target =  process.argv.slice(2)[0];
 const force = false;
 
 const { exec } = require("child_process");
